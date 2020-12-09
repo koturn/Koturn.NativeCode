eb664806385f4c6131b756af25d07a3dfd4815c9
koturn <jeak.koutan.apple@gmail.com>
2020-12-09 16:24:41 +0900

    Make properties in SystemInfo readonly

--- NativeCodeSharp/Internal/Win32/SystemInfo.cs
+++ NativeCodeSharp/Internal/Win32/SystemInfo.cs
@@ -14,54 +14,54 @@ namespace NativeCodeSharp.Internal.Win32
         /// <summary>
         /// The processor architecture of the installed operating system.
         /// </summary>
-        public ProcessorArchitecture ProcessorArchitecture { get; private set; }
+        public ProcessorArchitecture ProcessorArchitecture { get; }
         /// <summary>
         /// This member is reserved for future use.
         /// </summary>
-        private ushort Reserved { get; set; }
+        private ushort _reserved;
         /// <summary>
         /// <para>The page size and the granularity of page protection and commitment.</para>
         /// <para>This is the page size used by the VirtualAlloc function.</para>
         /// </summary>
-        public uint PageSize { get; private set; }
+        public uint PageSize { get; }
         /// <summary>
         /// A pointer to the lowest memory address accessible to applications and dynamic-link libraries (DLLs).
         /// </summary>
-        public IntPtr MinimumApplicationAddress { get; private set; }
+        public IntPtr MinimumApplicationAddress { get; }
         /// <summary>
         /// A pointer to the highest memory address accessible to applications and DLLs.
         /// </summary>
-        public IntPtr MaximumApplicationAddress { get; private set; }
+        public IntPtr MaximumApplicationAddress { get; }
         /// <summary>
         /// <para>A mask representing the set of processors configured into the system.</para>
         /// <para>Bit 0 is processor 0; bit 31 is processor 31.</para>
         /// </summary>
-        public IntPtr ActiveProcessorMask { get; private set; }
+        public IntPtr ActiveProcessorMask { get; }
         /// <summary>
         /// <para>The number of logical processors in the current group.</para>
         /// <para>To retrieve this value, use the GetLogicalProcessorInformation function.</para>
         /// </summary>
-        public uint NumberOfProcessors { get; private set; }
+        public uint NumberOfProcessors { get; }
         /// <summary>
         /// <para>An obsolete member that is retained for compatibility.</para>
         /// <para>Use the wProcessorArchitecture, wProcessorLevel, and wProcessorRevision members to determine the type of processor.</para>
         /// </summary>
-        public ProcessorType ProcessorType { get; private set; }
+        public ProcessorType ProcessorType { get; }
         /// <summary>
         /// <para>The granularity for the starting address at which virtual memory can be allocated.</para>
         /// <para>For more information, see VirtualAlloc.</para>
         /// </summary>
-        public uint AllocationGranularity { get; private set; }
+        public uint AllocationGranularity { get; }
         /// <summary>
         /// <para>The architecture-dependent processor level.</para>
         /// <para> It should be used only for display purposes.</para>
         /// <para>To determine the feature set of a processor, use the IsProcessorFeaturePresent function.</para>
         /// </summary>
-        public ushort ProcessorLevel { get; private set; }
+        public ushort ProcessorLevel { get; }
         /// <summary>
         /// <para>The architecture-dependent processor revision.</para>
         /// <para>The following table shows how the revision value is assembled for each type of processor architecture.</para>
         /// </summary>
-        public ushort ProcessorRevision { get; private set; }
+        public ushort ProcessorRevision { get; }
     }
 }
